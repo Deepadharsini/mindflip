73ecc8ee071273958233b7b614c0e50aba89cf14
davhsi <davish.std@gmail.com>
2025-02-10 09:49:37 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,6 @@ mongoose
 // Routes
 app.use("/api", authRoutes);
 
-// Server
+// Server`
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
